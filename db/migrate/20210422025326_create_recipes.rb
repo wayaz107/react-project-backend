5f7b6fecd40fa47e59f9b9ad6d893de12a3503c8
Wardayaz <wayaz107@gmail.com>
2021-04-21 22:49:14 -0500

    created migration

--- db/migrate/20210422025326_create_recipes.rb
+++ db/migrate/20210422025326_create_recipes.rb
@@ -1,7 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
-
+      t.string :name
+      t.string :description
+      t.string :image_url
+      
       t.timestamps
     end
   end
--- db/migrate/20210422033015_create_ingredient_recipes.rb
+++
@@ -1,8 +0,0 @@
-class CreateIngredientRecipes < ActiveRecord::Migration[6.0]
-  def change
-    create_table :ingredient_recipes do |t|
-
-      t.timestamps
-    end
-  end
-end
