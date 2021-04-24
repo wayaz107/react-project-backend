95b2e89d649f787cf2080abc5d589c60c97c6c80
Wardayaz <wayaz107@gmail.com>
2021-04-24 00:42:42 -0500

    added product controller actions

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::CommentsController < ApplicationController
+
+
+
 end
--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,29 @@
 class Api::V1::ProductsController < ApplicationController
+
+def index
+    products = Product.all
+    render json: products
+
+end 
+
+def show
+    product = Product.find(params[:id])
+    render json: product
+end 
+
+def update
+    product = Product.find(params[:id])
+    product.update(product_params)
+    render json: product
+
+end 
+
+private
+
+def product_params
+ params.require(:product).permit(:name, :brand, :description, :image, :likes)
+end 
+
+
+
 end
